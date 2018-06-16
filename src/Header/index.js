39ac62c727b5004e8fd39ecebca13df49fe6624f
Deepak Sisodiya <deepak.sisodiya@gmail.com>
2018-06-16 16:22:33 +0530

    added Name input in header

--- src/Header/index.js
+++ src/Header/index.js
@@ -1,6 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Header.css';
 
-const Header = () => <header className="Header" />;
+export default class Header extends Component {
+  state = {
+    name: ''
+  };
 
-export default Header;
+  handleChange = event => {
+    this.setState({ name: event.target.value });
+    event.preventDefault();
+  };
+
+  render() {
+    const { name } = this.state;
+
+    return (
+      <header className="Header">
+        <form>
+          <input
+            type="text"
+            value={name}
+            onChange={this.handleChange}
+            name="name"
+            placeholder="Enter Name"
+            className="name-input"
+          />
+        </form>
+      </header>
+    );
+  }
+}
