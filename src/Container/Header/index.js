c7dec8858e39f60fa8a411260f143276e1b08c1e
Deepak Sisodiya <deepak.sisodiya@gmail.com>
2018-06-16 21:22:26 +0530

    added input field in header for sorting users

--- src/Container/Header/index.js
+++ src/Container/Header/index.js
@@ -5,26 +5,41 @@ import './Header.css';
 
 export default class Header extends Component {
   state = {
-    name: ''
+    name: '',
+    sortValue: ''
   };
 
   handleChange = event => {
-    const { value } = event.target;
+    const { value, name } = event.target;
 
     this.setState({
-      name: value
+      [name]: value
     });
 
-    this.props.getAllUsers(value);
+    if (name === 'name') {
+      this.props.getAllUsers(value);
+    }
+
     event.preventDefault();
   };
 
   render() {
-    const { name } = this.state;
+    const { name, sortValue } = this.state;
 
     return (
       <header className="Header">
         <form>
+          <select
+            value={sortValue}
+            onChange={this.handleChange}
+            name="sortValue"
+            className="sort-input"
+          >
+            <option value="a-z">Name(A-Z)</option>
+            <option value="z-a">Name(Z-A)</option>
+            <option value="rankUp">Rank ↑</option>
+            <option value="rankDown">Rank ↓</option>
+          </select>
           <input
             type="text"
             value={name}
