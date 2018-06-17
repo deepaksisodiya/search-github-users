143cf9bb7b74b8c43ed5ef38292cda284c61256b
Deepak Sisodiya <deepak.sisodiya@gmail.com>
2018-06-17 21:08:49 +0530

    initial pagination code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Paginate from 'react-paginate';
 
 import Header from './Container/Header';
 import UsersList from './Components/UsersList';
@@ -49,6 +50,10 @@ class App extends Component {
     }
   }
 
+  onPageChange(pageObj) {
+    console.log(pageObj.selected + 1);
+  }
+
   render() {
     const { data, isLoading, isError } = this.state;
 
@@ -56,6 +61,12 @@ class App extends Component {
       <div className="App">
         <Header getAllUsers={this.getAllUsers} />
         <UsersList data={data} isLoading={isLoading} isError={isError} />
+        <Paginate
+          containerClassName="paginate-container"
+          previousClassName="prev-next-button"
+          nextClassName="prev-next-button"
+          onPageChange={this.onPageChange}
+        />
       </div>
     );
   }
